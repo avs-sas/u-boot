599d36c0e5a3f637ce4956d2f97eaa8878061c79
Michael Trimarchi <michael@amarulasolutions.com>
2024-06-25 11:46:29 +0200

    board/msc: Add common framework used to load module parameters
    The msc common allow you to load som module configuration. The
    implementation take in account only the minimal support for imx8mm
    module
    
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>
    Signed-off-by: Tommaso Merciai <tomm.merciai@gmail.com>

---
+++ board/msc/common/mx8m_common.c
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2019 AVNET
+ */
+
+#include <asm/arch/sys_proto.h>
+#include "mx8m_common.h"
+
+const char* mx8m_get_plat_str(void)
+{
+	switch (get_cpu_type()) {
+	case MXC_CPU_IMX8MM:
+		return "imx8mm";
+	}
+
+	return "N/A";
+}
+
+const char* mx8m_get_proc_str(void)
+{
+	switch (get_cpu_type()) {
+	case MXC_CPU_IMX8MM:
+		return "qc";
+	}
+	return "N/A";
+}
